df37365bc455fe65ad712af7c70b4bb419960fba
Pradeep <iampradeephr@gmail.com>
2022-10-31 09:41:13 +0530

    With Thread Communication with buffer size 1

--- Company.java
+++ Company.java
@@ -1,14 +1,26 @@
 class Company{
 
     int n;
-
+    boolean flag=false;
+    //flag-false (Producer will produce)
+    //flag-true  (Consumer will consume)
     public synchronized void produce_item(int n){
+        if(flag){
+            try { wait(); } catch (Exception e) {}
+        }
         this.n=n;
         System.out.println("Produced :"+n);
+        flag=!flag;
+        notify();
     }
 
     public synchronized int consume_item(){
+        if(!flag){
+            try { wait(); } catch (Exception e) {}
+        }
         System.out.println("Consumed :"+n);
+        flag=!flag;
+        notify();
         return n;
     }
 }
